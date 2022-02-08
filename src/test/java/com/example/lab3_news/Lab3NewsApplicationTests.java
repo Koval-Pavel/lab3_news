46088fb4c477a0f55e2634464ce4b4294ab8fedd
Koval-Pavel <koval_pv@ukr.net>
2022-02-08 12:39:02 +0200

    Initial commit

---
+++ src/test/java/com/example/lab3_news/Lab3NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3NewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
